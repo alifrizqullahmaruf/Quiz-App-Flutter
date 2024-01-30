9a668b23f7f5fffbc5009cb31919c58395ad5aa3
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-01-30 08:48:44 +0700

    Bagian edit text dan button belum

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,65 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/data/questions.dart';
 import 'package:second_app/question_screen.dart';
 import 'package:second_app/result_screen.dart';
 import 'package:second_app/start_screen.dart';
-import 'package:second_app/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   @override
-  State<Quiz> createState() => _QuizState();
+  State<Quiz> createState() {
+    return _QuizState();
+  }
 }
 
 class _QuizState extends State<Quiz> {
-  // buat variabel berisi item jawaban yang di click selectedAnswers
   List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = 'question-screen';
+      activeScreen = 'questions-screen';
     });
   }
 
-  // buat fungsi masukin answe ke list variabel selectedAnswers
-  void choseAnswer(String answer) {
+  void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
 
-    // membuat logic for page tampilan sehingga jetika tampilan sudah sama dengan index maka akan pindah ke page rasult
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        activeScreen = 'result-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     Widget screenWidget = StartScreen(switchScreen);
 
-    if (activeScreen == 'question-screen') {
+    if (activeScreen == 'questions-screen') {
       screenWidget = QuestionScreen(
-        onSelectAnswer: choseAnswer,
+        onSelectAnswer: chooseAnswer,
       );
     }
-    // ini merubah page ke page result 
-    if (activeScreen == 'result-screen') {
-      screenWidget = ResultScreen(chosenAnswers: selectedAnswers);
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
     }
 
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Colors.deepPurple,
-                Colors.deepPurple.shade300,
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
               ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
           child: screenWidget,
