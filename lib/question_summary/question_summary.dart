0dbdf26f96e2bd4c4e8eda1fecbff12dc3841ac6
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-02-01 09:39:48 +0700

    Add some resultitem and get design on it, tinggal tombol restart

--- lib/question_summary/question_summary.dart
+++ lib/question_summary/question_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/question_summary/summary_item.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
@@ -8,34 +9,12 @@ class QuestionsSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 400,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map(
             (data) {
-              return Row(
-                children: [
-                  Text(((data['question_index'] as int) + 1).toString(),style: const TextStyle(backgroundColor: Colors.purple,),),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text(
-                          data['question'] as String,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            color: Colors.white60,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Text(data['user_answer'] as String, style: TextStyle(color: Colors.purple.shade700),),
-                        Text(data['correct_answer'] as String,style: TextStyle(color: Colors.blue.shade700),)
-                      ],
-                    ),
-                  ),
-                ],
-              );
+              return SummaryItem(data);
             },
           ).toList(),
         ),
